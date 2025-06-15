6ed72a328e31b9db60b0c664ab20c07e0c565f96
wott12826 <freshplayer430@gmail.com>
2025-06-15 13:12:05 +0300

    Add Tailwind CSS and PostCSS dependencies, and configure build script

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
